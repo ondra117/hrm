3462f47c67a66b5b7058e1b00b33027c91def2f5
ondra117 <ondrabaudisch@gmail.com>
2025-10-14 11:52:28 +0200

    #

--- models/hrm/hrm_act_v1.py
+++ models/hrm/hrm_act_v1.py
@@ -275,9 +275,10 @@ class HierarchicalReasoningModel_ACTV1(nn.Module):
         seq_size = batch["inputs"].shape[1]
 
         return HierarchicalReasoningModel_ACTV1Carry(
-            inner_carry=eo.repeat(
-                self.inner.get_carry(), "... -> b s ...", b=batch_size, s=seq_size
-            ),
+            inner_carry=[
+                eo.repeat(c, "... -> b s ...", b=batch_size, s=seq_size)
+                for c in self.inner.get_carry()
+            ],
             steps=jnp.zeros((batch_size,), dtype=jnp.int32),
             halted=jnp.ones((batch_size,), dtype=jnp.bool_),
             current_data=jax.tree.map(jnp.zeros_like, batch),
